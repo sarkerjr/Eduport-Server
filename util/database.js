d4edf36483b2b68b8b60ba996bb6ad6415406665
Anik Sarker <sarkerjr@gmail.com>
2022-11-17 12:30:39 +0600

    before cpanel deployment

--- util/database.js
+++ util/database.js
@@ -1,33 +1,18 @@
-const Sequelize = require("sequelize");
-
-// const sequelize = new Sequelize(
-//     process.env.DATABASE_NAME,
-//     process.env.DATABASE_USERNAME,
-//     process.env.DATABASE_PASSWORD,
-//     {
-//         dialect: "postgres",
-//         host: process.env.DATABASE_HOST,
-//         port: process.env.DATABASE_PORT,
-//         ssl: true, 
-//         dialectOptions: {
-//             ssl: { require: true, rejectUnauthorized: false },
-//         },
-//     }
-// );
+const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(
-    "deld9f6o4kh8ts",
-    "qywvjakumbixkg",
-    "2deeeb8774fac76005b899c1aec754dda6c1e7aadd50d553bdccbbf160b6323c",
-    {
-        dialect: "postgres",
-        host: "ec2-3-229-161-70.compute-1.amazonaws.com",
-        port: 5432,
-        ssl: true,
-        dialectOptions: {
-            ssl: { require: true, rejectUnauthorized: false },
-        },
-    }
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USERNAME,
+  process.env.DATABASE_PASSWORD,
+  {
+    dialect: 'mysql',
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    ssl: true,
+    dialectOptions: {
+      ssl: { require: true, rejectUnauthorized: false },
+    },
+  }
 );
 
 module.exports = sequelize;
